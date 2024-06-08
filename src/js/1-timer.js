8fe4ab160917aa8961486c19975620fced77ac60
OlenaZib <155896018+OlenaZib@users.noreply.github.com>
2024-06-08 18:55:11 +0200

    upd

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,34 +2,60 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
 let userSelectedDate;
+let countdownInterval;
 
 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        userSelectedDate = selectedDates[0];
+        const countdownInterval = userSelectedDate - new Date();
+
+        if (countdownInterval < 0) {
+               iziToast.error({
+        color: 'red',
+        position: 'topRight',
+        message: 'Please choose a date in the future',
+      });
+            
+            startBtn.disabled = true;
+            inputTime.disabled = false;
+        } else {
+            startBtn.disabled = false;
+            inputTime.disabled = true;
+        }
+    },
 };
 
+const flatpickrInstance = flatpickr('#datetime-picker', options);
+const inputTime = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const timeValues = document.querySelectorAll('.value');
 
+startBtn.disabled = true;
 
+startBtn.addEventListener('click', () => {
+  countdownInterval = setInterval(() => {
+    const timeInterval = userSelectedDate - new Date();
+    
+    if (timeInterval < 0) {
+      clearInterval(countdownInterval);
+      startBtn.disabled = true;
+      inputTime.disabled = false;
+      return;
+    }
 
-
-
-
-
-
-
-
-
-
+    const { days, hours, minutes, seconds } = convertMs(timeInterval);
+    updateTimerDisplay({ days, hours, minutes, seconds });
+  }, 1000);
+});
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
@@ -43,6 +69,17 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000));
-console.log(convertMs(140000)); 
-console.log(convertMs(24140000)); 
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  timeValues[0].textContent = String(days).padStart(2, '0');
+  timeValues[1].textContent = String(hours).padStart(2, '0');
+  timeValues[2].textContent = String(minutes).padStart(2, '0');
+  timeValues[3].textContent = String(seconds).padStart(2, '0');
+}
+
+
+
+
+
+
+
+
